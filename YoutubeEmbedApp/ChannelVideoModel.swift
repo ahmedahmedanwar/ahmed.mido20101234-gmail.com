ba34c3abfce2a2e74a5228e1d803b4f17a572851
ahmed <ahmed.mido20101234@gmail.com>
2020-03-27 04:46:43 +0200

    rearrange

---
+++ YoutubeEmbedApp/ChannelVideoModel.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVideoModel.swift
+//  YoutubeEmbedApp
+//
+//  Created by Ahmed on 3/27/20.
+//  Copyright © 2020 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+
+class Video:NSObject {
+    var Key:String = ""
+    var Title:String = ""
+    var videoThumbnail:String = ""
+}
+
+
--- YoutubeEmbedApp/ViewController.swift
+++ YoutubeEmbedApp/ViewController.swift
@@ -20,48 +20,6 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
         // Do any additional setup after loading the view, typically from a nib.
         request()
         
-        let video = Video()
-            video.Key = "WEzVpZXyTZ0"
-            video.Title = "iOS 13.4 is Out! - What's New?"
-            videos.append(video)
-            
-            
-            let video2 = Video()
-            video2.Key = "SKBGvV9LW58"
-            video2.Title = "iPhone 11: My Honest Opinion After Two Months"
-            videos.append(video2)
-            
-            
-            let video3 = Video()
-            video3.Key = "hjfUrKxM1ds"
-            video3.Title = "Fake IPhone BETTER than the REAL????"
-            videos.append(video3)
-            
-            
-            let video4 = Video()
-            video4.Key = "v_5KcoSwmg4"
-            video4.Title = "Steve Jobs PISSED OFF moments 1997 2010"
-            videos.append(video4)
-            
-            
-            
-            let video5 = Video()
-            video5.Key = "sX1Y2JMK6g8"
-            video5.Title = "SpaceX Falcon Heavy- Elon Musk's Engineering Masterpiece"
-            
-            videos.append(video5)
-            
-            let video6 = Video()
-            video6.Key = "wbSwFU6tY1c"
-            video6.Title = "Falcon Heavy Test Flight"
-            videos.append(video6)
-            
-            
-            let video7 = Video()
-            video7.Key = "hh6xTguRn9o"
-            video7.Title = "Bill Gates explains how vaccines work"
-            
-            videos.append(video7)
             
     }
 
@@ -135,11 +93,6 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
         }
     }
     
-}
-
-class Video{
-    var Key:String = ""
-    var Title:String = ""
 }
 
 extension UIImageView {
